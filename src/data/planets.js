fd73d27e26eb1cf9fa95e886969772eaf3567682
MagoValdivia28 <felipepedro@aluno.senai.br>
2024-03-25 01:57:53 -0300

    Adiciona dados do planeta Terra ao arquivo de planetas

--- src/data/planets.js
+++ src/data/planets.js
@@ -1,1 +1,12 @@
-export 
+export const planetdata ={
+  id: 1,
+  name: "Terra",
+  date: "25/06/2006",
+  cor1: "verde",
+  cor2: "branco",
+  população: "8 Bilhões",
+  galaxia: "andromeda", 
+  sistemasolar: "Sim",
+  coordenadasespaciais: "41 24.2028, 2 10.4418",
+  governante: "Felipe"
+};
