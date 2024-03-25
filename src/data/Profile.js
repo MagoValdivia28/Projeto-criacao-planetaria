091eadb22ff94cbadf401257013553685cb5cf16
MagoValdivia28 <felipepedro@aluno.senai.br>
2024-03-25 02:00:26 -0300

    Adiciona perfil do Eu ao arquivo Profile.js

---
+++ src/data/Profile.js
@@ -0,0 +1,8 @@
+const Eu = {
+    name: 'Eu',
+    date: '25/06/2006',
+    email: 'felipe@gmail.com',
+    phone: '19 99999-9999',
+    description: "Sou um aluno do SENAI Valinhos cursando Desenvolvimento de Sistemas e, até o momento, estou completamente satisfeito com a experiência. Este curso tem sido uma fonte inesgotável de aprendizado, me proporcionando conhecimentos que nunca imaginei adquirir e habilidades que jamais pensei ser capaz de dominar. Estou verdadeiramente feliz com a oportunidade de estar aqui e poder aprender com os melhores professores e colegas de classe."
+};
+ export default Eu;
