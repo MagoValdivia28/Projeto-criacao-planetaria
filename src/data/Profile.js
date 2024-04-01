6b52d1a8888494990787abfa8d9a7cf6ea218219
MagoValdivia28 <felipepedro@aluno.senai.br>
2024-03-31 22:35:03 -0300

    Atualizar informações de perfil

--- src/data/Profile.js
+++ src/data/Profile.js
@@ -1,8 +1,8 @@
 const Eu = {
-    name: 'Eu',
+    name: 'Felipe da Silva Pedro',
     date: '25/06/2006',
-    email: 'felipe@gmail.com',
-    phone: '19 99999-9999',
+    email: 'felipeDaLuana@gmail.com',
+    phone: '19 4002-8922',
     description: "Sou um aluno do SENAI Valinhos cursando Desenvolvimento de Sistemas e, até o momento, estou completamente satisfeito com a experiência. Este curso tem sido uma fonte inesgotável de aprendizado, me proporcionando conhecimentos que nunca imaginei adquirir e habilidades que jamais pensei ser capaz de dominar. Estou verdadeiramente feliz com a oportunidade de estar aqui e poder aprender com os melhores professores e colegas de classe."
 };
  export default Eu;
